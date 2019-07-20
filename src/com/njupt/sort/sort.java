2c92d1059b0cd45b1af3535e8690885512e77cd7
zhangqiao <1053373312@qq.com>
2019-07-20 16:13:42 +0800

    直接插入排序   选择排序  希尔排序  计数排序 桶排序  基数排序

--- src/com/njupt/sort/sort.java
+++ src/com/njupt/sort/sort.java
@@ -1,5 +1,11 @@
 package com.njupt.sort;
 
+import org.omg.CORBA.INTERNAL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by zhangqiao on 2019/4/10.
  */
@@ -20,6 +26,23 @@ public class sort {
                 }
             }
             a[j+1] = value;
+        }
+    }
+
+    /**
+     * 选择排序
+     * */
+    public static void chooseSort(int a[]){
+        for(int i = 0;i < a.length;i++){
+            int min = i;//最小元素的下标
+            for(int j = i+1;j <a.length;j++){
+                if(a[j] < a[min]){
+                    min = j;
+                }
+            }
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
         }
     }
 
@@ -111,15 +134,177 @@ public class sort {
      * 堆排序
      */
 
+    /**
+     * 希尔排序
+     * */
+    public static void shellSort(int a[]){
+        int length = a.length;
+        //区间
+        int gap = 1;
+        while (gap < length) {
+            gap = gap * 3 + 1;
+        }
+        while (gap > 0) {
+            for (int i = gap; i < length; i++) {
+                int tmp = a[i];
+                int j = i - gap;
+                //跨区间排序
+                while (j >= 0 && a[j] > tmp) {
+                    a[j + gap] = a[j];
+                    j -= gap;
+                }
+                a[j + gap] = tmp;
+            }
+            gap = gap / 3;
+        }
+    }
+    /**
+     * 计数排序
+     * */
+    public static void jishuSort(int a[]){
+       int max = a[0];
+       for(int i = 1;i < a.length;i++){
+           if(a[i] > max){
+               max = a[i];
+           }
+       }
+
+       int[] countArr = new int[max+1];
+
+       for(int i = 0; i < a.length;i++){
+           countArr[a[i]]++;
+           a[i] = 0;
+       }
+
+       int index = 0;
+       for (int i = 0;i < countArr.length;i++){
+           if (countArr[i] > 0){
+               a[index++] = i;
+           }
+       }
+    }
+
+    /**
+     * 桶排序
+     * */
+    public static void tongSort(int[] a){
+        int min = a[0];
+        int max = a[0];
+        int len = a.length;
+        for(int i = 0; i < len;i++){
+            if(a[i] > max){
+                max = a[i];
+            }else if(a[i] < min){
+                min = a[i];
+            }
+        }
+
+        //桶的定义
+        ArrayList<ArrayList<Integer>> tong = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < len;i++){
+            tong.add(new ArrayList<>());
+        }
+        //每个桶的存储区间
+        float section = (float) (max-min)/(float)(len - 1);
+
+        //数据入桶
+        for(int i = 0; i < len;i++){
+            //桶的下标
+            int num = (int) (a[i]/section)-1;
+            if(num < 0){
+                num = 0;
+            }
+            tong.get(num).add(a[i]);
+        }
+
+        //桶内排序
+        for(int i = 0; i < tong.size();i++){
+            Collections.sort(tong.get(i));
+        }
+
+        int index = 0;
+        for (ArrayList<Integer> list:tong) {
+            for (int value:list) {
+                a[index] = value;
+                index++;
+            }
+        }
+
+
+    }
+    /**
+     * 基数排序
+     * */
+    public static void CardinalitySort(int[] a){
+        int len = a.length;
+        int max = a[0];
+        for(int i = 0; i < len;i++){
+            if(a[i] > max){
+                max = a[i];
+            }
+        }
+
+        int position = 1;
+
+        ArrayList<ArrayList<Integer>> tong = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < 10;i++){
+            tong.add(new ArrayList<Integer>());
+        }
+
+        while (true){
+            int num = (int) Math.pow(10,position-1);
+            if(max < num){
+                break;
+            }
+
+            //数据入桶
+            for(int i = 0; i < len; i++){
+                int yushu = ((a[i]/num)%10);
+                tong.get(yushu).add(a[i]);
+            }
+
+            //写回数组
+            int index = 0;
+            for(int i = 0;i < 10;i++){
+                int size = tong.get(i).size();
+                for(int j = 0;j < size;j++){
+                    a[index++] = tong.get(i).get(j);
+                }
+                tong.get(i).clear();
+            }
+            position++;
+
+        }
+    }
+
     public static void main(String[] args){
         int a[] = {4,6,3,2,5,8};
-        insertionSort(a);
-        /*quickSort(a,0,a.length-1);
-        for (int num:a) {
-            System.out.print(num);
-        }*/
+        /*插排*/
+        //insertionSort(a);
+
+        /*快排*/
+        //quickSort(a,0,a.length-1);
+
+        /*冒泡*/
         //bubble_sort(a);
+
+        /*归并排序*/
         //mergeSort(a,0,a.length-1);
+
+        /*选排*/
+        //chooseSort(a);
+
+        /*希尔排序*/
+        //shellSort(a);
+
+        /*计数排序*/
+        //jishuSort(a);
+
+        /*桶排序*/
+        //tongSort(a);
+
+        /*基数排序*/
+        CardinalitySort(a);
         for (int num:a) {
             System.out.print(num);
         }
